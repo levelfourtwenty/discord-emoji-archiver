5c1d811efc7de6e813303ec9c6e7b427bee4d7d2
levelfourtwenty <65911884+levelfourtwenty@users.noreply.github.com>
2022-07-28 01:10:22 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,6 +16,9 @@ async def on_ready():
 async def on_message(message):
         if message.content == "YOUR SECRET STRING HERE":
                 await message.delete()
+                dirname = message.guild.name
+                dirname = dirname.replace(" ", "_")
+                os.mkdir(dirname)
                 totallength = len(message.guild.emojis)
                 currentemoji = 0
                 for i in message.guild.emojis:
@@ -24,9 +27,9 @@ async def on_message(message):
                         req = requests.get(i.url)
                         match i.animated:
                                 case True:
-                                        open(f"{i.name}.gif", "wb").write(req.content)
+                                        open(f"{dirname}/{i.name}.gif", "wb").write(req.content)
                                 case False:
-                                        open(f"{i.name}.png", "wb").write(req.content)
+                                        open(f"{dirname}/{i.name}.png", "wb").write(req.content)
                         print(f"Finished downloading {i.name} [{currentemoji}/{totallength}].\n")
 
 
